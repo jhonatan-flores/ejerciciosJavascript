0bb1f6fdaae6f712e571192c7ae94b73695f449f
jhonatan-flores <henrryflorescupe@gmail.com>
2024-10-30 12:56:36 -0500

    ejercicio 7

---
+++ ejercicios/7.Contar_rango_de_numeros/ejercicio.js
@@ -0,0 +1,12 @@
+function contarRango(num1, num2) {
+    let contador = 0;
+    for (let i = num1 + 1; i < num2; i++) {
+        contador++;
+    }
+    return contador;
+}
+
+// código de prueba
+console.log(contarRango(1, 9)); // 7
+console.log(contarRango(1332, 8743)); // 7410
+console.log(contarRango(5, 6)); // 0
