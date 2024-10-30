3322e1dcafb89d36628a5267c2f702b7554d5c0d
jhonatan-flores <henrryflorescupe@gmail.com>
2024-10-30 12:52:36 -0500

    ejercicio 4

---
+++ ejercicios/4.imprimir_un_arreglo/ejercicio.js
@@ -0,0 +1,10 @@
+function imprimirArreglo(arr) {
+    arr.forEach(element => console.log(element));
+}
+
+// código de prueba
+imprimirArreglo([1, "Hola", 2, "Mundo"]);
+// 1
+// Hola
+// 2
+// Mundo
